c605ee254818a321c9f6153fe067878b9a49531f
Richard Osborne <richard@xmos.com>
2012-09-22 18:18:00 +0100

    Build LLVM using cmake.

--- cmake/External_llvm.cmake
+++ cmake/External_llvm.cmake
@@ -7,18 +7,19 @@ ExternalProject_Add(clang
   INSTALL_COMMAND ""
 )
 
-set(LLVM_CONFIGURE_ARGS --with-clang-srcdir=${CMAKE_CURRENT_BINARY_DIR}/external/src/clang)
+list(APPEND llvm_build_vars
+  "-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/external")
+list(APPEND llvm_build_vars
+  "-DLLVM_CLANG_SOURCE_DIR:PATH=${CMAKE_CURRENT_BINARY_DIR}/external/src/clang")
 
 ExternalProject_Add(llvm
   DEPENDS clang
   PREFIX external
   URL http://llvm.org/releases/3.1/llvm-3.1.src.tar.gz
   URL_MD5 16eaa7679f84113f65b12760fdfe4ee1
-  PATCH_COMMAND ${LLVM_PATCH_COMMAND}
-  CONFIGURE_COMMAND ../llvm/configure ${LLVM_CONFIGURE_ARGS} --prefix=${CMAKE_CURRENT_BINARY_DIR}/external
-  INSTALL_COMMAND make install
+  CMAKE_CACHE_ARGS ${llvm_build_vars}
 )
 
 set(LLVM_CONFIG_EXECUTABLE ${CMAKE_CURRENT_BINARY_DIR}/external/bin/llvm-config)
-set(CLANG_EXECUTABLE ${CMAKE_CURRENT_BINARY_DIR}/external/bin/clang)
-set(CLANGPLUSPLUS_EXECUTABLE ${CMAKE_CURRENT_BINARY_DIR}/external/bin/clang++)
+set(CLANG_EXECUTABLE ${CMAKE_CURRENT_BINARY_DIR}/external/bin/clang${CMAKE_EXECUTABLE_SUFFIX})
+set(CLANGPLUSPLUS_EXECUTABLE ${CMAKE_CURRENT_BINARY_DIR}/external/bin/clang++${CMAKE_EXECUTABLE_SUFFIX})
