d4eede700eee89f2dc3d8e295e214181615454a1
Richard Osborne <richard@xmos.com>
2013-04-23 20:43:57 +0100

    Revert "Update to LLVM 3.2" to work around issues on OS X. If the LLVM 3.2 headers are compiled under -std=c++11 they expect to find a definition of std::move in <utility>, but this requires also requires the -stdlib=libc++ option.
    This reverts commit 79e2b2d919ef21cc393a7d29bc3a7c4209ba0ef0.

--- cmake/External_llvm.cmake
+++ cmake/External_llvm.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(clang
   PREFIX external
-  URL http://llvm.org/releases/3.2/clang-3.2.src.tar.gz
-  URL_MD5 3896ef4334df08563b05d0848ba80582
+  URL http://llvm.org/releases/3.1/clang-3.1.src.tar.gz
+  URL_MD5 59bf2d3120a3805f27cafda3823caaf8
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
@@ -10,11 +10,12 @@ ExternalProject_Add(clang
 list(APPEND llvm_build_vars
   "-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}"
   "-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/external"
-  "-DLLVM_EXTERNAL_CLANG_SOURCE_DIR:PATH=${CMAKE_CURRENT_BINARY_DIR}/external/src/clang")
+  "-DLLVM_CLANG_SOURCE_DIR:PATH=${CMAKE_CURRENT_BINARY_DIR}/external/src/clang")
+
 ExternalProject_Add(llvm
   DEPENDS clang
   PREFIX external
-  URL http://llvm.org/releases/3.2/llvm-3.2.src.tar.gz
-  URL_MD5 71610289bbc819e3e15fdd562809a2d7
+  URL http://llvm.org/releases/3.1/llvm-3.1.src.tar.gz
+  URL_MD5 16eaa7679f84113f65b12760fdfe4ee1
   CMAKE_CACHE_ARGS ${llvm_build_vars}
 )
