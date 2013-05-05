403e1f8bf1c6704ed532fccf32326e9c8face1c3
Richard Osborne <richard@xmos.com>
2013-05-05 19:11:56 +0100

    Add MD5 for SDL.

--- cmake/External_SDL.cmake
+++ cmake/External_SDL.cmake
@@ -6,6 +6,7 @@ endif()
 ExternalProject_Add(SDL
   PREFIX external
   URL http://www.libsdl.org/release/SDL-1.2.15.tar.gz
+  URL_MD5 9d96df8417572a2afb781a7c4c811a85
   CONFIGURE_COMMAND ../SDL/configure ${SDL_configure_args} --prefix=${CMAKE_CURRENT_BINARY_DIR}/external
   INSTALL_COMMAND make install
 )
