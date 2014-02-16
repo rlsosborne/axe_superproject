09024e80cf239bee5cae3430efa67aa34a95e559
Richard Osborne <richard@xmos.com>
2014-02-16 11:23:57 +0000

    Use the correct standard library version on OS X.
    Fixes #1.

--- cmake/External_llvm.cmake
+++ cmake/External_llvm.cmake
@@ -17,8 +17,26 @@ list(APPEND llvm_build_vars
 # Workaround LLVM bug #16246 by avoiding AArch64 backend.
 list(APPEND llvm_build_vars "-DLLVM_TARGETS_TO_BUILD:STRING=X86")
 
+set(llvm_dependencies clang)
+
+if(APPLE)
+  # Install libc++ headers in the directory that clang expects to find them in.
+  # This matches what macports does, see https://trac.macports.org/ticket/34288.
+  ExternalProject_Add(libcxx
+    PREFIX external
+    URL http://llvm.org/releases/3.3/libcxx-3.3.src.tar.gz
+    URL_MD5 59006e659ffb33f5222a7b79d4cd071e
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND
+    ${CMAKE_COMMAND} -E make_directory <INSTALL_DIR>/lib/c++ &&
+    ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include <INSTALL_DIR>/lib/c++/v1
+  )
+  list(APPEND llvm_dependencies libcxx)
+endif()
+
 ExternalProject_Add(llvm
-  DEPENDS clang
+  DEPENDS ${llvm_dependencies}
   PREFIX external
   URL http://llvm.org/releases/3.3/llvm-3.3.src.tar.gz
   URL_MD5 40564e1dc390f9844f1711c08b08e391
