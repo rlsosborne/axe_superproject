a062ab85f4323fa246c392748c47afa650f7b9c8
Richard Osborne <richard@xmos.com>
2015-08-09 17:23:56 +0100

    Configure boost for an out of tree build.
    Also:
    
    - base the toolchain on ${CMAKE_CXX_COMPILER_ID}
    - use list(APPEND ...) where possible.

--- cmake/External_Boost.cmake
+++ cmake/External_Boost.cmake
@@ -1,10 +1,22 @@
 set(Boost_build_args --prefix=<INSTALL_DIR> link=static)
 
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-  set(Boost_build_args ${Boost_build_args} variant=debug)
+  list(APPEND Boost_build_args variant=debug)
 else()
-  set(Boost_build_args ${Boost_build_args} variant=release)
+  list(APPEND Boost_build_args variant=release)
 endif()
+
+if("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+  set(BOOST_BUILD_TOOLSET clang)
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+  set(BOOST_BUILD_TOOLSET msvc)
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+  set(BOOST_BUILD_TOOLSET gcc)
+else()
+  message(FATAL_ERROR "unsupported compiler \"${CMAKE_CXX_COMPILER_ID}\"")
+endif()
+
+list(APPEND Boost_build_args toolset=${BOOST_BUILD_TOOLSET})
 
 if(MINGW)
   if(${CMAKE_GENERATOR} STREQUAL "MSYS Makefiles")
@@ -12,25 +24,33 @@ if(MINGW)
   else()
     set(Boost_CONFIGURE_COMMAND bootstrap.bat mingw)
   endif()
-  set(Boost_build_args ${Boost_build_args} toolset=gcc)
-elseif(APPLE)
-  set(Boost_CONFIGURE_COMMAND ./bootstrap.sh --with-toolset=clang)
-  set(Boost_build_args ${Boost_build_args} toolset=clang)
-  # Workaround build issues with XCode 5 (see Boost Ticket #8507)
-  set(Boost_build_args ${Boost_build_args} cxxflags='-ftemplate-depth=256')
+elseif(MSVC)
+  set(Boost_CONFIGURE_COMMAND bootstrap.bat msvc)
 else()
-  set(Boost_CONFIGURE_COMMAND ./bootstrap.sh)
+  set(Boost_CONFIGURE_COMMAND ./bootstrap.sh --with-toolset=${BOOST_BUILD_TOOLSET})
+endif()
+
+if(APPLE)
+  # Workaround build issues with XCode 5 (see Boost Ticket #8507)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftemplate-depth=256")
+endif()
+
+if(${CMAKE_CXX_FLAGS})
+  list(APPEND Boost_build_args "cxxflags=${CMAKE_CXX_FLAGS}")
 endif()
 
 # Avoid building libraries with external dependencies.
-set(Boost_build_args ${Boost_build_args} --without-context --without-coroutine --without-iostreams --without-python)
+list(APPEND Boost_build_args
+     --without-context --without-coroutine --without-iostreams --without-python)
 
 ExternalProject_Add(Boost
   PREFIX external
   URL http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.bz2
   URL_MD5 b8839650e61e9c1c0a89f371dd475546
-  CONFIGURE_COMMAND ${Boost_CONFIGURE_COMMAND}
+  CONFIGURE_COMMAND
+    ${CMAKE_COMMAND} -E chdir <SOURCE_DIR> ${Boost_CONFIGURE_COMMAND}
   BUILD_COMMAND ""
-  INSTALL_COMMAND ./b2 ${Boost_build_args} install
-  BUILD_IN_SOURCE 1
+  INSTALL_COMMAND
+    ${CMAKE_COMMAND} -E chdir <SOURCE_DIR>
+    ./b2 --build-dir=<BINARY_DIR> ${Boost_build_args} install
 )
