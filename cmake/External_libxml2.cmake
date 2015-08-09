effb8a6f5476f82b094446f6eb1662b5f0b0082f
Richard Osborne <richard@xmos.com>
2015-08-09 17:03:22 +0100

    Update libxml2 / libxslt version.

--- cmake/External_libxml2.cmake
+++ cmake/External_libxml2.cmake
@@ -1,9 +1,11 @@
-set(libxml2_configure_args --disable-shared --without-iconv --without-zlib --without-threads --without-ftp --without-http)
+set(libxml2_configure_args --disable-shared --without-iconv --without-zlib
+    --without-threads --without-ftp --without-http --without-python)
 
 ExternalProject_Add(libxml2
   PREFIX external
-  URL ftp://xmlsoft.org/libxml2/libxml2-2.7.8.tar.gz
-  URL_MD5 8127a65e8c3b08856093099b52599c86
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure ${libxml2_configure_args} --prefix=<INSTALL_DIR>
+  URL ftp://xmlsoft.org/libxml2/libxml2-2.9.2.tar.gz
+  URL_MD5 9e6a9aca9d155737868b3dc5fd82f788
+  CONFIGURE_COMMAND
+    <SOURCE_DIR>/configure ${libxml2_configure_args} --prefix=<INSTALL_DIR>
   INSTALL_COMMAND make install
 )
--- cmake/External_libxslt.cmake
+++ cmake/External_libxslt.cmake
@@ -3,8 +3,8 @@ set(libxslt_configure_args --disable-shared --without-crypto --without-python --
 ExternalProject_Add(libxslt
   DEPENDS libxml2
   PREFIX external
-  URL ftp://xmlsoft.org/libxslt/libxslt-1.1.27.tar.gz
-  URL_MD5 4d01ff464b9ea7236925a1adf147c7f8
+  URL ftp://xmlsoft.org/libxslt/libxslt-1.1.28.tar.gz
+  URL_MD5 9667bf6f9310b957254fdcf6596600b7
   CONFIGURE_COMMAND <SOURCE_DIR>/configure ${libxslt_configure_args} --prefix=<INSTALL_DIR>
   INSTALL_COMMAND make install
 )
