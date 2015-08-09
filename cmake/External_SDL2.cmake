d4cff12d2624706dfe9eb161622757b379840620
Richard Osborne <richard@xmos.com>
2015-08-09 17:02:55 +0100

    Update SDL2 version.

--- cmake/External_SDL2.cmake
+++ cmake/External_SDL2.cmake
@@ -5,8 +5,8 @@ endif()
 
 ExternalProject_Add(SDL2
   PREFIX external
-  URL http://www.libsdl.org/release/SDL2-2.0.0.tar.gz
-  URL_MD5 beec89afb6edcc6f0abc4114f2e6bcf7
+  URL http://www.libsdl.org/release/SDL2-2.0.3.tar.gz
+  URL_MD5 fe6c61d2e9df9ef570e7e80c6e822537
   CONFIGURE_COMMAND <SOURCE_DIR>/configure ${SDL2_configure_args} --prefix=<INSTALL_DIR>
   INSTALL_COMMAND make install
 )
